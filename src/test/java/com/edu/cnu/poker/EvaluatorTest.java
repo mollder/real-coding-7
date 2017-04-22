e435f0ee1d18a503ef021819d639ecb238467ff4
mollder <mollder@naver.com>
2017-04-22 20:18:11 +0900

    commit test fourcard royalflush straightflush

--- src/test/java/com/edu/cnu/poker/EvaluatorTest.java
+++ src/test/java/com/edu/cnu/poker/EvaluatorTest.java
@@ -38,4 +38,43 @@ public class EvaluatorTest {
         String result = evaluator.evaluate(cardList);
         assertThat(result, is("FULLHOUSE"));
     }
+    @Test
+    public void 같은카드가_4개면_포카드다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(1, Suit.CLUBS),
+                new Card(1, Suit.DIAMONDS),
+                new Card(1, Suit.SPADES),
+                new Card(1, Suit.HEARTS),
+                new Card(3, Suit.SPADES)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("FOURCARD"));
+    }
+    @Test
+    public void 같은_짝패의_카드5장이_연속되면_스트레이트플러시다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(3, Suit.HEARTS),
+                new Card(4, Suit.HEARTS),
+                new Card(5, Suit.HEARTS),
+                new Card(6, Suit.HEARTS),
+                new Card(7, Suit.HEARTS)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("STRAIGHTFLUSH"));
+    }
+    @Test
+    public void 같은_짝패의_10에서_에이스까지_카드5장이_연속되면_로열플러시다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(10, Suit.HEARTS),
+                new Card(11, Suit.HEARTS),
+                new Card(12, Suit.HEARTS),
+                new Card(13, Suit.HEARTS),
+                new Card(14, Suit.HEARTS)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("ROYALFLUSH"));
+    }
 }
