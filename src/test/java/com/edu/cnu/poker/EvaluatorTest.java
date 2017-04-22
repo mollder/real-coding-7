b5fdc50aa190a785357d6d916d5bc73a4825605f
kcs93023 <kcs93023@gmail.com>
2017-04-22 21:05:18 +0900

    Make Test HIGH CARD

--- src/test/java/com/edu/cnu/poker/EvaluatorTest.java
+++ src/test/java/com/edu/cnu/poker/EvaluatorTest.java
@@ -50,4 +50,20 @@ public class EvaluatorTest {
         String result = evaluator.evaluate(cardList);
         assertThat(result, is("FOURCARD"));
     }
+
+    @Test
+    public void 어떤_조합도_가능하지_않으면_하이카드다(){
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(5, Suit.CLUBS),
+                new Card(13, Suit.DIAMONDS),
+                new Card(1, Suit.SPADES),
+                new Card(12, Suit.HEARTS),
+                new Card(7, Suit.SPADES),
+                new Card(6, Suit.HEARTS),
+                new Card(2, Suit.SPADES)
+        );
+        String result = evaluator.evaluate(cardList);
+        assertThat(result, is("HIGHCARD"));
+    }
 }
